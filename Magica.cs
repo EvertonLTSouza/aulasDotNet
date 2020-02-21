92b8b070aa042b54d69db70748fca3511f89e7c8
Everton Luis Teixeira de Souza <evertonts@ciandt.com>
2020-02-21 14:27:33 -0300

    Primeira aula

---
+++ Magica.cs
@@ -0,0 +1,8 @@
+public class Magica
+{
+    public int Soma (int val1, int val2)
+    {
+        int result = val1 + val2;
+        return result;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aulas_dotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Magica soma = new Magica();
+            Console.WriteLine("Digite o primeiro numero da soma: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());            
+            Console.WriteLine("Digite o primeiro numero da soma: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            
+            int result = soma.Soma(num1,num2);
+            
+            Console.WriteLine("A soma e igual a: " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
+
